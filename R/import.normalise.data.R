09721d912130178376c056b4624cc4ec3db7a3ed
Matina <fragkogs@mskcc.org>
2019-09-25 16:25:26 -0400

    Update import.normalise.data.R

--- R/import.normalise.data.R
+++ R/import.normalise.data.R
@@ -23,30 +23,30 @@ Import.normalise.data <- function(){
   require(ggrepel)
   
   #### Read sample information and raw counts
-  pData = read.delim(file = "phenoData/ALL_pData.txt", header = TRUE, stringsAsFactors = FALSE, sep = "\t")
-  pData = pData[which(pData$Batch_info %in% c("4","5","6")),]
-  pData = pData[which(pData$Condition %in% c("Breast_cancer","Normal")),]
-  pData$Condition2 = ifelse(pData$Condition == "Normal","Normal","Cancer")
+  pData <- read.delim(file = "phenoData/ALL_pData.txt", header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+  pData <- pData[which(pData$Batch_info %in% c("4","5","6")),]
+  pData <- pData[which(pData$Condition %in% c("Breast_cancer","Normal")),]
+  pData$Condition2 <- ifelse(pData$Condition == "Normal","Normal","Cancer")
   
   cat("Reading files in..")
-  d =readDGE(pData$Filenames, path = "Counts/Batch 1_3_4_5_6/", columns=c(1,2), pData$Condition2)
+  d <- readDGE(pData$Filenames, path = "Counts/Batch 1_3_4_5_6/", columns = c(1,2), pData$Condition2)
   d$samples$batch = pData$Batch_info
   
   #### Filter lowly expressed genes and recalculate library size
   cat("Filtering lowly expressed genes..")
-  keep = rowSums(cpm(d)>1) >= 32
-  d = d[keep,]
+  keep <- rowSums(cpm(d) > 1) >= 32
+  d <- d[keep,]
   print(table(keep))
-  colnames(d) = pData$SampleName
-  d$samples$libSize = colSums(d$counts)
+  colnames(d) <- pData$SampleName
+  d$samples$libSize <- colSums(d$counts)
   
   #### Upperquartile normalization and log2 transformation
   cat("Upperquartile normalization and log2 transformation..")
   d <- calcNormFactors(d, method = "upperquartile")
-  lcpm = cpm(d,log=TRUE,prior.count=1)
+  lcpm <- cpm(d,log = TRUE,prior.count = 1)
   
   #### Remove unecessery dots from the ensemble IDs
-  rownames(lcpm) = remove.dots(lcpm)
+  rownames(lcpm) <- remove.dots(lcpm)
   
   return(list("lcpm" = lcpm,"dge" = d))
 }
