6219d3e92cec10d80e55b9aea13acd8464d96c70
Matina <fragkogs@mskcc.org>
2019-09-25 16:25:45 -0400

    Update random_classifiers.R

--- R/random_classifiers.R
+++ R/random_classifiers.R
@@ -12,7 +12,7 @@
 source(file = "Classification_Analysis/tidy_code/random.class.perm.test.R")
 
 #### Run permutation testing for the training data 
-random.classifiers = random.class.perm.test(data_fr, rf.corr.rfe$optVariables, 1000)
+random.classifiers <- random.class.perm.test(data_fr, rf.corr.rfe$optVariables, 1000)
 
 #### The "random.class.perm.test()" function takes sometime to run, you can uncomment this for the pre-run data
 #load("~/repos_not_updated/nature_paper_code/Monocytes_analysis_2018/Classification_Analysis/tidy_code/random.classifiers_1000_permutations.RData")
@@ -26,11 +26,11 @@ quantile(random.classifiers$TrainAccuracy, probs = c(.025, 0.975))
 (sum( c(random.classifiers$TrainAccuracy, 0.8509) >= 0.8509) +1)/ (1000 +1) # one tailed test for training
 
 # print density plot and histogram for training accuracy
-df = data.frame("Performance" = random.classifiers$TrainAccuracy)
+df <- data.frame("Performance" = random.classifiers$TrainAccuracy)
 
-ggplot(df, aes(x=Performance, color = Performance)) +
-  geom_histogram(color="#1B9E77", fill="#1B9E77",bins = 20,alpha=0.5,position="identity") +
-  geom_vline(data=df, aes(xintercept=0.8509, color="red"),linetype="solid") +
-  labs(title="Random signatures (training set)", y = "Histogram") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  theme(plot.title = element_text(face="bold", size=20)) 
-
+ggplot(df, aes(x = Performance, color = Performance)) +
+  geom_histogram(color = "#1B9E77", fill = "#1B9E77",bins = 20,alpha = 0.5,position = "identity") +
+  geom_vline(data = df, aes(xintercept=0.8509, color = "red"),linetype = "solid") +
+  labs(title = "Random signatures (training set)", y = "Histogram") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  theme(plot.title = element_text(face = "bold", size = 20)) 
+ 
